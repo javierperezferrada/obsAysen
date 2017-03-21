c395f16528d8e1ec2400f18eb0e069bbe766acae
javier <javierperezferrada@gmail.com>
2017-03-21 11:22:57 -0300

    added carrousel to banner

--- gui/static/gui/js/scripts.js
+++ gui/static/gui/js/scripts.js
@@ -4,6 +4,11 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
 
+  	$('.carousel.carousel-slider').carousel({fullWidth: true});
+  	$('.carousel').carousel({dist:0});
+        window.setInterval(function(){$('.carousel').carousel('next')},10000)
+        
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 $( document ).ready(function() {
